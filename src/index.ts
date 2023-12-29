6d5b703fd3226502812bb15615e44579d4500cbe
Massimiliano Mirra <bard@hyperstruct.net>
2023-12-29 16:38:13 +0000

    prepare

--- src/index.ts
+++
@@ -1,9 +0,0 @@
-import { add } from "./operations.js";
-
-const main = (): void => {
-  const result = add(2, 4);
-  // eslint-disable-next-line no-console
-  console.log("result is ", result);
-};
-
-main();
